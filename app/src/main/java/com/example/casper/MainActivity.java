df036f0de69aaa9a5ec7807411ddcd7349a0393d
qq739163387 <739163387@qq.com>
2019-09-03 23:39:56 +0800

    通过findviewbyld获得textview控件，调用其settext方法设置其内容

--- app/src/main/java/com/example/casper/MainActivity.java
+++ app/src/main/java/com/example/casper/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.casper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textViewHelloWord;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewHelloWord=findViewById(R.id.textViewHelloWord);
+        textViewHelloWord.setText("你好，编程实现的Hello world！");
     }
 }
